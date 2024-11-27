150e8a478f531016e80532fedd616342c059e7c4
muhammadaliteacher <muhammadalishuhratjonov1122@gmail.com>
2024-11-27 17:39:03 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 const express = require("express")
 const DeviceDetector = require('node-device-detector');
-const connectDB = require("./db/config")
+const connectDB = require("./db/config");
 const User = require("./model/user.model")
 
 const app = express()
-const detector = new DeviceDetector()
-
-app.use(express.json())
 connectDB()
+app.use(express.json())
+
+const detector = new DeviceDetector()
 
 app.use(async (req, res, next) => {
   const userName = req.header("username")
@@ -20,25 +20,29 @@ app.use(async (req, res, next) => {
 
   const userAgent = req.headers["user-agent"]
   const foundedDevice = detector.detect(userAgent)
-  const deviceId = 
-  `${foundedDevice?.device?.os?.name}-${foundedDevice?.device?.type}-${foundedDevice?.client?.name}`
 
-  try {
+  const deviceId = 
+  `${foundedDevice?.device?.model}-${foundedDevice?.device?.brand}-${foundedDevice?.device?.type}-${foundedDevice?.client?.name}-${foundedDevice?.client?.short_name}-${foundedDevice?.os?.family}-${foundedDevice?.os?.name}`
+
+  try{
     let foundedUser = await User.findOne({username: userName})
+
     if(!foundedUser) {
-     foundedUser = new User({username: userName, devices: {deviceId, deviceName: foundedDevice?.device?.brand}})
+      foundedUser = new User({username: userName, devices: {deviceId, deviceName: foundedDevice?.device?.brand}})
     }else{
-      const exisitsDevice = foundedUser.devices.find((item) => item.deviceId === deviceId)
-      
-      if(!exisitsDevice) {
-        if(foundedUser.devices.length >= 3){
-          return res.json({
-            message: "Device limit exeeded! you cannot user more then 3 devices"
+      const existsDevice = foundedUser.devices.find((item) => item.deviceId === deviceId)
+
+      if(!existsDevice) {
+        if(foundedUser.devices.length <= 2){
+          foundedUser.devices.push({deviceId, deviceName: foundedDevice?.device?.brand})
+        }else{
+          res.json({
+            message: "Device limit exeeded! you cannot use more then 3 devices"
           })
         }
-        foundedUser.devices.push({deviceId, deviceName: foundedDevice?.device?.brand})
       }
     }
+    
     await foundedUser.save()
     next()
   }catch(error) {
@@ -47,7 +51,9 @@ app.use(async (req, res, next) => {
 })
 
 app.get("/", (req, res) => {
-  res.json({message: "Welcome to my app"})
+  res.json({
+    message: "Welecome to my app"
+  })
 })
 
 app.listen(3000, () => {
